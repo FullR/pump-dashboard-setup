d6efc82283519c12c751e04f5a19e0954ad7a345
James Meyers <James.Meyers919@gmail.com>
2016-10-16 22:16:15 -0700

    Added symlinks to node installation for root access. Changed start script

--- setup-beaglebone.sh
+++ setup-beaglebone.sh
@@ -4,30 +4,30 @@ promptTimezone() {
   sudo dpkg-reconfigure tzdata
 }
 
-enableRootSSH() {
-  sudo sed -i '/PermitRootLogin prohibit-password/c\PermitRootLogin yes' /etc/ssh/sshd_config
-}
-
 installNode() {
-  sudo apt-get install build-essential libssl-dev curl git vim -y
-  curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.32.0/install.sh | bash && source ~/.bashrc
-  $HOME/.nvm/nvm.sh
+  sudo apt-get update
+  sudo apt-get install build-essential libssl-dev curl git -y
+  curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.32.0/install.sh | bash
+  export NVM_DIR="$HOME/.nvm"
+  [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
   nvm install stable
-  source ~/.bashrc
+  # create symbolic links so root has access to the same versions of node and npm
+  sudo ln -s "$NVM_DIR/versions/node/$(nvm version)/bin/node" "/usr/bin/node"
+  sudo ln -s "$NVM_DIR/versions/node/$(nvm version)/bin/npm" "/usr/bin/npm"
 }
 
 installPostgre() {
   sudo apt-get install postgresql postgresql-contrib -y
-  # set postgres user's password to password00
   sudo -u postgres psql -c "ALTER USER \"postgres\" WITH PASSWORD 'foobar321'"
   sudo -u postgres psql -c 'CREATE DATABASE "pump-manager"'
 }
 
 setupPumpController() {
+  export BEAGLEBONE=true
+  export NODE_ENV=production
   echo "export BEAGLEBONE=true" >> ~/.bashrc
   echo "export NODE_ENV=production" >> ~/.bashrc
   # echo 'export PUMP_EMAIL_PASSWORD="email password goes here"' >> ~/.bashrc
-  source ~/.bashrc
   git clone https://github.com/FullR/pump-dashboard.git
   cd pump-dashboard
   git checkout rewrite-sep-2016
@@ -36,14 +36,14 @@ setupPumpController() {
 
   # create start script
   echo "#!/bin/bash" > start
-  echo "node pump-dashboard/server" >> start
+  echo "cd pump-dashboard && sudo npm run start:beaglebone" >> start
   chmod +x start
 }
 
-#assertRoot
-enableRootSSH
 sudo apt-get update
-promptTimezone
+sudo apt-get install vim -y
 installNode
 installPostgre
 setupPumpController
+promptTimezone
+passwd
